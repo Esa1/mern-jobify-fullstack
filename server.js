0929ae8b991d09cfe8491cc7081a8294035469e3
Esa Torkkila <esa.torkkila@gmail.com>
2026-02-28 13:10:19 +0200

    Set up env

--- server.js
+++ server.js
@@ -1,10 +1,29 @@
+import * as dotenv from "dotenv";
+dotenv.config();
 import express from "express";
 const app = express();
+import morgan from "morgan";
+
+//console.log("NODE_ENV=");
+//console.log(process.env.NODE_ENV);
+if (process.env.NODE_ENV === "development") {
+  //  console.log("still running morgan");
+  app.use(morgan("dev"));
+}
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello world");
 });
 
-app.listen(5100, () => {
-  console.log("server running");
+app.post("/", (req, res) => {
+  console.log(req);
+  res.json({ message: "data received", data: req.body });
+});
+
+const port = process.env.port || 5100;
+
+app.listen(port, () => {
+  console.log(`server running on port ${port}...`);
 });
