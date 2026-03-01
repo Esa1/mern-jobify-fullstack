5b33a1bf116c64d07e4bfcf27c2bea4a424604b8
Esa Torkkila <esa.torkkila@gmail.com>
2026-03-01 16:30:20 +0200

    Add get single job route

--- server.js
+++ server.js
@@ -33,7 +33,6 @@ app.get("/api/v1/jobs", (req, res) => {
 
 // Create job
 app.post("/api/v1/jobs", (req, res) => {
-  console.log("create job");
   const { company, position } = req.body;
   if (!company || !position) {
     res.status(400).json({ msg: "please provide company and position" });
@@ -42,7 +41,18 @@ app.post("/api/v1/jobs", (req, res) => {
   const id = nanoid(10);
   const job = { id, company, position };
   jobs.push(job);
-  console.log(jobs);
+  res.status(201).json({ job });
+});
+
+// Get single job ()
+app.get("/api/v1/jobs/:id", (req, res) => {
+  console.log("Get single job");
+  const { id } = req.params;
+  const job = jobs.find((job) => job.id === id);
+  if (!job) {
+    return res.status(404).json({ msg: `no job with id ${id}` });
+  }
+
   res.status(200).json({ job });
 });
 
