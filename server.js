65691cd6125a7bca59c2736418cd167b6d807ce7
Esa Torkkila <esa.torkkila@gmail.com>
2026-03-01 14:18:30 +0200

    Add get jobs route

--- server.js
+++ server.js
@@ -3,18 +3,13 @@ dotenv.config();
 import express from "express";
 const app = express();
 import morgan from "morgan";
+import { nanoid } from "nanoid";
 
-try {
-  const response = await fetch(
-    "https://www.course-api.com/react-useReducer-cart-project",
-  );
-  const cartData = await response.json();
-  console.log(cartData);
-} catch (error) {
-  console.log(error);
-}
-//console.log("NODE_ENV=");
-//console.log(process.env.NODE_ENV);
+let jobs = [
+  { id: nanoid(), company: "apple", position: "front-end" },
+  { id: nanoid(), company: "google", position: "back-end" },
+];
+
 if (process.env.NODE_ENV === "development") {
   //  console.log("still running morgan");
   app.use(morgan("dev"));
@@ -29,6 +24,10 @@ app.get("/", (req, res) => {
 app.post("/", (req, res) => {
   console.log(req);
   res.json({ message: "data received", data: req.body });
+});
+
+app.get("/api/v1/jobs", (req, res) => {
+  res.status(200).json({ jobs });
 });
 
 const port = process.env.port || 5100;
