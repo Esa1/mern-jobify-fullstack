c257f652154849c780b92f1efbc32d8341c91ae7
Esa Torkkila <esa.torkkila@gmail.com>
2026-03-02 14:23:23 +0200

    Add delete job route and finish basic CRUD app.

--- server.js
+++ server.js
@@ -76,6 +76,20 @@ app.patch("/api/v1/jobs/:id", (req, res) => {
   res.status(200).json({ msg: "job modified", job });
 });
 
+// Delete job ()
+app.delete("/api/v1/jobs/:id", (req, res) => {
+  console.log("Edit job");
+  const { id } = req.params;
+  const job = jobs.find((job) => job.id === id);
+  if (!job) {
+    return res.status(404).json({ msg: `no job with id ${id}` });
+  }
+
+  const newJobs = jobs.filter((job) => job.id !== id);
+  jobs = newJobs;
+  res.status(200).json({ msg: "job deleted" });
+});
+
 const port = process.env.port || 5100;
 
 app.listen(port, () => {
