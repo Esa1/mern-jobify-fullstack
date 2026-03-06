b803930fb919a2abf86d4a865bf453f107fe4a49
Esa Torkkila <esa.torkkila@gmail.com>
2026-03-06 11:16:36 +0200

    Set up test route for testing Express validator

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ import express from "express";
 const app = express();
 import morgan from "morgan";
 import mongoose from "mongoose";
+import { body, validationResult } from "express-validator";
+
 // routers
 import jobRouter from "./routes/jobRouter.js";
 //middleware
@@ -22,10 +24,22 @@ app.get("/", (req, res) => {
   res.send("Hello world");
 });
 
-app.post("/", (req, res) => {
-  console.log(req);
-  res.json({ message: "data received", data: req.body });
-});
+app.post(
+  "/api/v1/test",
+  [body("name").notEmpty().withMessage("name is required")],
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      const errorMessages = errors.array().map((error) => error.msg);
+      return res.status(400).json({ errors: errorMessages });
+    }
+    next();
+  },
+  (req, res) => {
+    const { name } = req.body;
+    res.json({ message: `hello ${name}` });
+  },
+);
 
 app.use("/api/v1/jobs", jobRouter);
 
