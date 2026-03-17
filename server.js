b5026829bf84d75d44196c8a4626a4b863e3193c
Esa Torkkila <esa.torkkila@gmail.com>
2026-03-17 15:53:04 +0200

    User routes

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ import cookieParser from "cookie-parser";
 // routers
 import jobRouter from "./routes/jobRouter.js";
 import authRouter from "./routes/authRouter.js";
+import userRouter from "./routes/userRouter.js"
 
 //middleware
 import errorHandlerMiddleware from "./middleware/errorHandlerMiddleware.js";
@@ -27,12 +28,8 @@ app.get("/", (req, res) => {
   res.send("Hello world");
 });
 
-// app.post("/api/v1/test", validateTest, (req, res) => {
-//   const { name } = req.body;
-//   res.json({ message: `hello ${name}` });
-// });
-
 app.use("/api/v1/jobs", authenticateUser, jobRouter);
+app.use("/api/v1/users", authenticateUser, userRouter);
 app.use("/api/v1/auth", authRouter);
 
 // Not found middleware for handling non-existing routes
