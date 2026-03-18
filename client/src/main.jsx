b5280a4a7c24bd666bb57cd18051268537c0b9c8
Esa Torkkila <esa.torkkila@gmail.com>
2026-03-18 15:41:19 +0200

    Axios and finish backend part

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,14 +2,19 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+// import axios from "axios";
+import customFetch from "./utils/customFetch.js";
 
+// const resp = await axios.get("/api/v1/test");
+const resp = await customFetch.get("/test");
+console.log(resp);
 // fetch("http://localhost:5100/api/v1/test")
-fetch("/api/v1/test")
-  .then((res) => res.json())
-  .then((data) => {
-    console.log(data);
-    console.log("test123");
-  });
+// fetch("/api/v1/test")
+//   .then((res) => res.json())
+//   .then((data) => {
+//     console.log(data);
+//     console.log("test123");
+//   });
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
