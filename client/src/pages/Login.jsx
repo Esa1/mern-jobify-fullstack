15914fa328a1fed15e7828a22352eb31bcf97030
Esa Torkkila <esa.torkkila@gmail.com>
2026-03-30 20:17:47 +0300

    useActionsData example

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,5 +1,11 @@
 import React from "react";
-import { Form, Link, redirect, useNavigation } from "react-router-dom";
+import {
+  Form,
+  Link,
+  redirect,
+  useNavigation,
+  useActionData,
+} from "react-router-dom";
 import Wrapper from "../assets/wrappers/RegisterAndLoginPage";
 import { FormRow, Logo } from "../components";
 import customFetch from "../utils/customFetch";
@@ -8,19 +14,26 @@ import { toast } from "react-toastify";
 export const action = async ({ request }) => {
   const formData = await request.formData();
   const data = Object.fromEntries(formData);
-  console.log("in login action");
+  const errors = { msg: "" };
+  if (data.password.length < 3) {
+    errors.msg = "password too short";
+    return errors;
+  }
+
   try {
     await customFetch.post("/auth/login", data);
     toast.success("Login succeeded");
     return redirect("/dashboard");
   } catch (error) {
-    toast.error(error?.response?.data?.msg);
+    //    toast.error(error?.response?.data?.msg);
+    errors.msg = error?.response?.data?.msg; //useActionData example
     return error;
   }
   return null;
 };
 
 const Login = () => {
+  const errors = useActionData();
   const navigation = useNavigation();
   const isSubmitting = navigation.state === "submitting";
 
@@ -29,13 +42,14 @@ const Login = () => {
       <Form method="post" className="form">
         <Logo />
         <h4>login</h4>
-        <FormRow
-          type="email"
-          name="email"
-          defaultValue="esa.torkkila@gmail.com"
-        />
+        {errors?.msg && <p style={{ color: "red" }}>{errors.msg}</p>}{" "}
+        <FormRow type="email" name="email" defaultValue="john@gmail.com" />
         <FormRow type="password" name="password" defaultValue="secret123" />
-        <button type="submit" className="btn btn-block">
+        <button
+          type="submit"
+          className="btn btn-block"
+          isdisabled={isSubmitting}
+        >
           {isSubmitting ? "submitting" : "submit"}
         </button>
         <button type="button" className="btn btn-block">
