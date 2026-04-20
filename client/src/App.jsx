e55fa5ed429355676924eec560746e55670b10c9
Esa Torkkila <esa.torkkila@gmail.com>
2026-04-20 19:38:40 +0300

    Start admin page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -22,6 +22,7 @@ import { loader as allJobsLoader } from "./pages/AllJobs";
 import { action as editJobAction } from "./pages/EditJob";
 import { loader as editJobLoader } from "./pages/EditJob";
 import { action as deleteJobAction } from "./pages/DeleteJob";
+import { loader as adminLoader } from "./pages/Admin";
 
 export const checkDefaultTheme = () => {
   const isDarkTheme = localStorage.getItem("darkTheme") === "true";
@@ -70,6 +71,7 @@ const router = createBrowserRouter([
           {
             path: "admin",
             element: <Admin />,
+            loader: adminLoader,
           },
           {
             path: "edit-job/:id",
--- client/src/pages/Admin.jsx
+++ client/src/pages/Admin.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import { useLoaderData, redirect } from "react-router-dom";
+import customFetch from "../utils/customFetch";
+import Wrapper from "../assets/wrappers/StatsContainer";
+import { toast } from "react-toastify";
+
+export const loader = async () => {
+  try {
+    const response = await customFetch.get("/users/admin/app-stats");
+    return response.data;
+  } catch (error) {
+    toast.error("You are not authorized to access this page");
+    return redirect("/dashboard");
+  }
+};
 
 const Admin = () => {
-  return <h1>Admin page</h1>;
+  const data = useLoaderData();
+
+  return (
+    <Wrapper>
+      <h1>Admin page</h1>
+    </Wrapper>
+  );
 };
 
 export default Admin;
