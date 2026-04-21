2f797fe27a012b184324685fb4a562197282230f
Esa Torkkila <esa.torkkila@gmail.com>
2026-04-21 14:16:29 +0300

    User schema update for avatar

--- models/UserModel.js
+++ models/UserModel.js
@@ -17,12 +17,14 @@ const UserSchema = new mongoose.Schema({
     enum: ["user", "admin"],
     default: "user",
   },
+  avatar: String,
+  avatarPublicId: String,
 });
 
-UserSchema.methods.toJSON = function() {
-  let obj = this.toObject()
-  delete obj.password
-  return obj
-}
+UserSchema.methods.toJSON = function () {
+  let obj = this.toObject();
+  delete obj.password;
+  return obj;
+};
 
 export default mongoose.model("User", UserSchema);
