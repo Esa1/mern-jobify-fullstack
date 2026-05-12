cbf36dcfa92f8fe1cc6159ef75e4e8437c136f6e
Esa Torkkila <esa.torkkila@gmail.com>
2026-05-12 15:51:23 +0300

    Setup pagination logic - first approach

--- client/src/components/PageBtnContainer.jsx
+++ client/src/components/PageBtnContainer.jsx
@@ -1,15 +1,32 @@
 import { HiChevronDoubleLeft, HiChevronDoubleRight } from "react-icons/hi";
 import Wrapper from "../assets/wrappers/PageBtnContainer";
+import { useLocation, Link, useNavigate } from "react-router-dom";
 import { useAllJobsContext } from "../pages/AllJobs";
+
 const PageBtnContainer = () => {
   const {
     data: { numOfPages, currentPage },
   } = useAllJobsContext();
   const pages = Array.from({ length: numOfPages }, (_, index) => index + 1);
-  console.log(pages);
+
+  const { search, pathname } = useLocation();
+  const navigate = useNavigate();
+  console.log(search, pathname);
+
+  const handlePageChange = (pageNumber) => {
+    // Implement page change logic here, e.g., update the URL with the new page number
+    const searchParams = new URLSearchParams(search);
+    searchParams.set("page", pageNumber);
+    navigate(`${pathname}?${searchParams.toString()}`);
+  };
+
   return (
     <Wrapper>
-      <button className="btn prev-btn">
+      <button
+        className="btn prev-btn"
+        onClick={() => handlePageChange(currentPage - 1)}
+        disabled={currentPage === 1}
+      >
         <HiChevronDoubleLeft />
         prev
       </button>
@@ -19,13 +36,18 @@ const PageBtnContainer = () => {
             <button
               key={pageNumber}
               className={`btn page-btn ${currentPage === pageNumber && "active"}`}
+              onClick={() => handlePageChange(pageNumber)}
             >
               {pageNumber}
             </button>
           );
         })}
       </div>
-      <button className="btn next-btn">
+      <button
+        className="btn next-btn"
+        onClick={() => handlePageChange(currentPage + 1)}
+        disabled={currentPage === numOfPages}
+      >
         next
         <HiChevronDoubleRight />
       </button>
