0e3967fcc9a6a9376c0e8e7a1bc983ebd0d61ebf
Esa Torkkila <esa.torkkila@gmail.com>
2026-05-12 14:41:26 +0300

    Start render buttons

--- client/src/components/PageBtnContainer.jsx
+++ client/src/components/PageBtnContainer.jsx
@@ -1,11 +1,24 @@
-import React from "react";
+import { HiChevronDoubleLeft, HiChevronDoubleRight } from "react-icons/hi";
+import Wrapper from "../assets/wrappers/PageBtnContainer";
 import { useAllJobsContext } from "../pages/AllJobs";
 const PageBtnContainer = () => {
   const {
     data: { numOfPages, currentPage },
   } = useAllJobsContext();
   console.log(numOfPages, currentPage);
-  return <div>PageBtnContainer</div>;
+  return (
+    <Wrapper>
+      <button className="btn prev-btn">
+        <HiChevronDoubleLeft />
+        prev
+      </button>
+      <div className="btn-container"></div>
+      <button className="btn next-btn">
+        <HiChevronDoubleRight />
+        next
+      </button>
+    </Wrapper>
+  );
 };
 
 export default PageBtnContainer;
